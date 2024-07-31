10e0c964efe41c1ad51b7a6343ae9212c56c0892
jholson1030 <jholson1030@gmail.com>
2024-07-31 11:19:58 -0500

    Add font awesome script and declare variables

---
+++ script.js
@@ -0,0 +1,7 @@
+// Variables
+
+const display = document.querySelector('.display');
+const startButton = document.querySelector('.start');
+const stopButton = document.querySelector('.stop');
+const resetButton = document.querySelector('.reset');
+
