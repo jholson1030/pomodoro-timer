863be68f8cdc54485a3523d074ca157ea3ca0ff9
jholson1030 <jholson1030@gmail.com>
2024-07-31 12:06:12 -0500

    Declare timer variables

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const startButton = document.querySelector('.start');
 const stopButton = document.querySelector('.stop');
 const resetButton = document.querySelector('.reset');
 
+
+// Timer variables
+let timerDuration = 25 * 60; // 25 minutes in seconds
+let remainingTime = timerDuration;
+let timerInterval;
+
+let breakDuration = 5 * 60; // 5 minutes in seconds
+let remainingBreakTime = breakDuration;
+let breakInterval;
+
